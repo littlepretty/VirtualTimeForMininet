49a033dd7bab85b50fa7ce67eb079c307e26c046
littlepretty <littlepretty881203@gmail.com>
2015-07-02 18:58:43 +0800

    sync test script
    Signed-off-by: littlepretty <littlepretty881203@gmail.com>

--- mininet/mininet/vt_experiments/ExperimentStringScaleTDF/vtmn_scalability.py
+++ mininet/mininet/vt_experiments/ExperimentStringScaleTDF/vtmn_scalability.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python
 
 """
-Benchmarking experiment for fidelity
+Test bandwidth (using iperf) on string/chain networks of varying size,
+using both kernel and user datapaths.
 
-Test bandwidth (using iperf) on string/chain networks of fixed size 40,
-using kernel datapaths.
-
-First construct a network of 2 hosts and N switches, connected as follows:
+We construct a network of 2 hosts and N switches, connected as follows:
 
        h1 - s1 - s2 - ... - sN - h2
 
 
-Varying link bw, with & without virtual time, test throughput between h1 and h2
-"""
+WARNING: by default, the reference controller only supports 16
+switches, so this test WILL NOT WORK unless you have recompiled
+your controller to support 100 switches (or more.)
 
+In addition to testing the bandwidth across varying numbers
+of switches, this example demonstrates:
+
+- creating a custom topology, StringTestTopo
+- using the ping() and iperf() tests from Mininet()
+- testing both the kernel and user switches
+
+"""
 import sys
 import numpy
 import time
+import timeit
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as pyplot
@@ -28,16 +36,15 @@ from mininet.log import lg
 from mininet.util import irange, custom
 from mininet.node import CPULimitedHost
 from mininet.link import TCLink
-from mininet.cli import CLI
 from functools import partial
 from mininet.clean import cleanup
 
 flush = sys.stdout.flush
 
-class StringTestTopo(Topo):
+class StringTestTopo( Topo ):
     "Topology for a string of N switches and 2 hosts."
 
-    def __init__(self, N, **params):
+    def __init__( self, N, **params ):
 
         # Initialize topology
         Topo.__init__( self, **params )
@@ -58,105 +65,124 @@ class StringTestTopo(Topo):
             last = switch
 
 
-def stringBandwidthTest(host_class, controller_class, link_class, size, tdf, data_file):
+def stringBandwidthTest(length, host_class, controller_class, link_class, size, tdf, data_file):
 
     "Check bandwidth at various lengths along a switch chain."
 
+    switchCount = size
+
     topo_class = StringTestTopo(size)
 
-    net = Mininet(topo=topo_class, host=host_class, switch=OVSKernelSwitch, controller=controller_class, waitConnected=True, link=link_class)
-    # no tdf_adaptor to change TDF
+    net = Mininet( topo=topo_class, host=host_class, switch=OVSKernelSwitch, controller=controller_class, waitConnected=True, link=link_class)
     net.start()
 
+    src, dst = net.hosts[ 0 ], net.hosts[ 1 ]
+    print "*** testing basic connectivity"
+    net.ping( [ src, dst ] )
     
-    print "*** testing basic connectivity\n"
-    src, dst = net.hosts
     if tdf == 1:
         num_pings = 3
         for i in irange(1, num_pings):
-            ping_result = list(net.pingFull( [ src, dst ] ))
-            # ping_result=[(host1), (host2)]
-            # host = (src, dst, data)
-            # data = (#sent, #received, rttmin, rttavg, rttmax, rttdev)
-            print "Ping avg rtt = %s\n" % ping_result[0][2][3]
+            ping_result = list(net.pingFull( [src, dst] ))
             rttavg = ping_result[0][2][3]
-        data_file.write( "RTT Avg = %s ms\n" % rttavg)
-    else:
-        net.ping( [src, dst] )
+        data_file.write("%s\t" % rttavg)
+        data_file.flush()
 
-    print "*** testing bandwidth\n"
+    print "*** testing bandwidth"
+    print "testing", src, "<->", dst
+
     num_rounds = 3
-    client_history = []
-    time = 16
+    #cli_results = []
+    ser_results = []
     omit = 1
+    time = 11
+    start = timeit.default_timer()
     for i in irange(1, num_rounds):
-        # bandwidth = net.iperf( [src, dst], l4Type = 'UDP', udpBw='%sM'%set_bw, format = 'm', time=20, clifile=data_file, serfile=data_file )
-        bandwidth = net.iperf( [src, dst], l4Type = 'TCP', format = 'm', time=time, omit=omit, clifile=data_file, serfile=data_file )
+        bandwidth = net.iperf([src, dst], l4Type = 'TCP',  format = 'm', time=time, omit=omit)
         flush()
         serout = bandwidth[0]
         cliout = bandwidth[1]
 
-        if len(serout) > 0 and len(cliout) > 0:
+        if len(serout) > 0:
             serDataStr, unit = serout.split(" ")
             serData = float(serDataStr)
+            if serData > 0.1:
+                ser_results.append(serData)
 
-            cliDataStr, unit = cliout.split(" ")
-            cliData = float(cliDataStr)
-            client_history.append(cliData)
-            data_file.write("%s\t%f\t%f\t%s\t%s\n" % (size, src.tdf, net.cpu_usage, serData, cliData))
+        #if len(cliout) > 0:
+            #cliDataStr, unit = cliout.split(" ")
+            #cliData = float(cliDataStr)
+            #if cliData > 0.1:
+                #cli_results.append( cliData )
 
-    client_mean = numpy.mean(client_history)
-    client_stdev = numpy.std(client_history)
-    data_file.write( "Avg Throughtput = %f\n" % client_mean )
-    data_file.write( "STD Throughput = %f\n" % client_stdev )
-    print "AVG = %f " % client_mean
-    print "STD = %f " % client_stdev
-    data_file.write('\n\n')
+    end = timeit.default_timer()
+    elapsed = end - start
+    print "elapsed: %s\n" % elapsed
+    # unit = Mbits/sec
+    #avgCliBw = numpy.mean( cli_results )
+    #stdevCliBw = numpy.std( cli_results )
+    maxSerBw = numpy.amax(ser_results)
+    minSerBw = numpy.amin(ser_results)
+    if len(ser_results) >= 10:
+        ser_results.remove(maxSerBw)
+        ser_results.remove(minSerBw)
+
+    avgSerBw = numpy.mean(ser_results)
+    stdevSerBw = numpy.std(ser_results)
+
+    #data_file.write("%s\t%s\t%s\t%s\t%s\n" % (size, avgCliBw, stdevCliBw, maxCliBw, minCliBw))
+    data_file.write("%s\t%s\t%s\t%s\n" % (size, avgSerBw, stdevSerBw, elapsed))
+    data_file.flush()
     net.stop()
-    return client_mean, client_stdev
+    return avgSerBw, stdevSerBw
 
-def runTest(file_name, controller, tdf, size, set_cpu, set_bw, set_delay="10us"):
+def runTest(file_name, controller, tdf, size, set_cpu, set_bw, set_delay="50us"):
     lg.setLogLevel( 'info' )
 
     """in fact, Controller and Remotecontroller have no difference
     all we need to do is start or not start POX in another shell"""
     if controller == "POX":
-        controller = partial( RemoteController, ip = '127.0.0.1', port=6633 )
+        controller = partial( RemoteController, ip = '127.0.0.1', port = 6633 )
     else:
         controller = DefaultController
-    link = partial( TCLink, bw=set_bw, delay=set_delay )
+    link = partial( TCLink, bw = set_bw, delay = set_delay )
 
     """config host's cpu share and time dilation factor"""
-    host = custom(CPULimitedHost, sched='cfs', period_us=100000, cpu=set_cpu, tdf=tdf)
+    host = custom( CPULimitedHost, sched = 'cfs', period_us = 100000, cpu = 0.5, tdf = TDF )
 
     """with w option, it automatically overwrite everytime"""
     data_file = open('%s.log' % file_name, 'w')
     print "Results are written to %s.log file" % file_name
-    data_file.write("********* Running stringBandwidthTest *********\n")
+
+    data_file.write("********* Server String Topowith TDF = %d *********\n" % TDF)
+
+    if tdf == 1:
+        data_file.write("RTT\tSwitchCount\tAvg\tStd\tTime\n")
+    else:
+        data_file.write("SwitchCount\tAvg\tStd\tTime\n")
     data_file.flush()
 
-    # seems mininet cannot handle more than 640 switches
-    print "******* Running with %d switches, TDF = %d *******" % (size, tdf)
-    client_avg, client_stdev = stringBandwidthTest(host, controller, link, size, tdf, data_file)
+    print "********* Running stringBandwidthTest *********", size
+    avg, std = stringBandwidthTest(size)
+
     cleanup()
-    return client_avg, client_stdev
+    return avg, std
 
-
-def drawData(output, AvgRates, StdRates, BWsInGb):
-    base_category = (1, 2, 3)
+def drawData(output, AvgRates, StdRates, SwSizes):
+    base_category = (1, 2, 3, 4, 5)
     dataLables = ['Mininet, TDF=1', 'Mininet, TDF=4', 'Physical Testbed']
-    xLabel = 'Link Bandwidth (Gbps)'
+    xLabel = '#Switches'
     yLabel = 'Average TCP Throughput (Gbps)'
 
     color_list = ['c', 'r', 'm', 'y', 'g', 'b', 'k', 'w']
     hatch_list = ['/', '\\', '+', 'x', 'o', '.', '*', '-']
-    width = 0.2
+    width = 0.1
     fontSize = 21
-    maxY = 10
+    maxY = 5
 
     rects = []
     fig, ax = pyplot.subplots()
-    for index in [0, 1, 2]:
+    for index in [0, 1, 2, 3, 4]:
         category = [x + index * width for x in base_category]
         rect = ax.bar(category, AvgRates[index], width, color=color_list[index], yerr=StdRates[index], hatch=hatch_list[index])
         rects.append(rect)
@@ -164,7 +190,7 @@ def drawData(output, AvgRates, StdRates, BWsInGb):
     ax.legend(tuple(rects), dataLables, shadow=True, fancybox=True, fontsize=fontSize, loc='upper left')
     ax.set_xticks([x + width*3/2 for x in base_category ])
     # ax.set_xticklabels(('4', '8', '10'))
-    ax.set_xticklabels(BWsInGb)
+    ax.set_xticklabels(SwSizes)
     ax.set_yticks(range(maxY+1))
 
     for tick in ax.xaxis.get_major_ticks():
@@ -188,38 +214,24 @@ def main():
     AvgRates = []
     StdRates = []
     TDFs = [1, 4]
-    BWs = [1000, 2000, 4000]
-    size = 16
+    Sizes = [10, 20, 40, 60, 80]
+    BW = 2000
     for tdf in TDFs:
         avg_rates = []
         std_rates = []
-        for bw in BWs:
-            file_name = "PerfStringBW%dMTDF%d" %(bw, tdf)
-            avg, std = runTest(file_name, "NO", tdf, size, 0.5, bw)
+        for size in Sizes:
+            file_name = "ScaleStringSw%dTDF%d" %(size, tdf)
+            avg, std = runTest(file_name, "NO", tdf, size, 0.5, BW)
             # convert to Gbps
             avg_rates.append(avg / 1024)
             std_rates.append(std / 1024)
         AvgRates.append(avg_rates)
         StdRates.append(std_rates)
 
-    # trust me, I got them from physical testbed
-    testbed_avg_rates = [3.78, 7.42, 9.22]
-    testbed_std_rates = [0.06, 0.147, 0.239]
-    AvgRates.append(avg_rates)
-    StdRates.append(testbed_std_rates)
+    Sizes2Str = [str(x) for x in Sizes]
+    drawData('ScaleBw%dDiffSz' % BW, AvgRates, StdRates, tuple(Sizes2Str))
 
-    BWsInGb = [ str(x/1000) for x in BWs]
-    drawData('Perf%dSwDiffBw' % size, AvgRates, StdRates, tuple(BWsInGb))
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
 
